5ad23434252a89e81ca4dee2a25ca5be3e6283db
Morinke01 <ejulius250@gmail.com>
2023-11-30 11:11:00 +0300

    Assignment completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word) -> None:
+        self.word = word
+
+    def is_anagram(self, word2):
+        return sorted(self.word) == sorted(word2)
+
+    def match(self, candidate_list):
+        correct_anagrams = [candidate for candidate in candidate_list if self.is_anagram(candidate)]
+        if correct_anagrams:
+            print(f"The correct anagram(s) for '{self.word}' is/are: {correct_anagrams}")
+        else:
+            return []
+        return correct_anagrams
